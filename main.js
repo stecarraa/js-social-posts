aab43b238e3c2a448113738f5b31ebf2c3f2a2e9
stecarraa <stefano@caratozzolo.me>
2022-06-30 16:22:25 +0200

    not working milestone 1

--- main.js
+++ main.js
@@ -123,3 +123,47 @@ const posts = [
         "created": "2021-12-11"
     }
 ];
+
+const postsList = document.getElementsByClassName('posts-list');
+let post = '';
+
+
+const { id, content, media, author, likes, is_liked, created } = posts;
+    
+for (let i = 0; i < posts.length; i++) {
+    post += `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${author.image}" alt="Phil Mangione">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${author.name}</div>
+                <div class="post-meta__time">${created}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${content}.</div>
+    <div class="post__image">
+        <img src="${media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div>
+    </div>
+</div>
+
+    `
+}
+
+postsList.innerHTML += post;
